93a621e2080e84075e3bc2c43167122528713f10
leo <zhij_web@163.com>
2018-04-21 18:05:41 +0800

    modify

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 var express = require('express')
+var compression = require('compression')
 var axios = require('axios')
 
-var app = express()
+var port = process.env.PORT || 9000
 
-app.get('/api/getData', function (req, res) {
+var app = express()
+var apiRoutes = express.Router()
+
+apiRoutes.get('/api/getData', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*')
     var url = 'http://api.steampowered.com/IDOTA2Match_570/GetLeagueListing/v1?key=6D7A52CD3D21C352CE3AC33A2DCF1A78'
     axios.get(url).then(function (response) {
         res.send(response.data)
     })
-}).listen(3000, function () {
-    console.log('server is running at http://127.0.0.1:3000')
+})
+app.use('/api', apiRoutes)
+
+app.use(compression())
+
+app.use(express.static('./dist'))
+
+module.exports = app.listen(port, function (err) {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log('Listening at http://localhost:' + port + '\n')
 })
